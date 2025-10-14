109bbca015a203089e939e4eacf629ce27ba00fb
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-10-14 21:06:41 +0530

    Chnages for day 2

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
+  console.log(process.env.JWT_SECRET, "- process.env.JWT_SECRET");
+  console.log(process.env.MONGODB_URL, "- process.env.MONGODB_URL");
   res.send("Welcome to the Express server!");
 });
 
 app.get("/payments/get", (req, res) => {
+  console.log(process.env.JWT_SECRET, "- process.env.JWT_SECRET");
+  console.log(process.env.MONGODB_URL, "- process.env.MONGODB_URL");
   res.send("swaraj!");
 });
 
 app.get("/cart/one", (req, res) => {
+  console.log(process.env.JWT_SECRET, "- process.env.JWT_SECRET");
+  console.log(process.env.MONGODB_URL, "- process.env.MONGODB_URL");
   res.send("two!");
 });
 
