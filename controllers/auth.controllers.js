f8c0d50ada21a764208587c8ecdab9e91570f374
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-11-01 12:32:00 +0530

    Updates

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,13 +1,50 @@
 import User from "../models/user.schema.js";
+import bcrypt from "bcrypt";
 
-export const Login = (req, res) => {
-  res.send("Login Route");
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and Password are required", success: false });
+    }
+    const isUserExists = await User.findOne({ email: email });
+
+    console.log(isUserExists, "isUserExists");
+    if (!isUserExists) {
+      return res
+        .status(404)
+        .json({ message: "Email not found", success: false });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      isUserExists.password
+    );
+    console.log(isPasswordValid, "isPasswordValid");
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: "Invalid Password", success: false });
+    }
+    return res.status(200).json({
+      message: "Login Successful",
+      success: true,
+      user: { name: isUserExists.name, userId: isUserExists._id },
+    });
+  } catch (error) {
+    console.log("error", error);
+    res
+      .status(500)
+      .send({ message: "Error in Registering user", success: false });
+  }
 };
 
 export const Register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    
+
     // console.log(name, email, password);
 
     // const isUserExist = User.find({ email: "virat@gmail.com" });
@@ -16,15 +53,24 @@ export const Register = async (req, res) => {
     const isUserExistForOne = await User.findOne({ email: email });
     console.log(isUserExistForOne, "isUserExistForOne");
     if (isUserExistForOne) {
-      return res.status(400).send({ message: "Email already exists." });
+      return res
+        .status(400)
+        .json({ message: "Email already exists.", success: false });
     }
-    const newUser = User({ name: name, email, password });
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    // console.log(encryptedPassword, "encryptedPassword", password);
+    const newUser = User({ name: name, email, password: encryptedPassword });
     // console.log(newUser, "newUser");
     await newUser.save();
-    res.status(201).send({ message: "User Registered Successfully" });
+    res
+      .status(201)
+      .json({ message: "User Registered Successfully", success: true });
   } catch (error) {
     console.log("error", error);
-    res.status(500).send({ message: "Error in Registering user" });
+    res
+      .status(500)
+      .send({ message: "Error in Registering user", success: false });
   }
 };
 
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import mainRouter from "./routes/index.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,7 +15,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1", mainRouter);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => console.log("DB Connected!"));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB Connected!"));
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
